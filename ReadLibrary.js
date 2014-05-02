e4e3d5d7583c93b7b70a48967be8b284c98d45d7
Ricky54326 <mutschlechne@wisc.edu>
2014-05-02 01:12:29 -0500

    mitch halp pls

--- ReadLibrary.js
+++ ReadLibrary.js
@@ -97,6 +97,8 @@ function mainFunc(){
     var subgenres = {};
 	var artists = {};
 
+    var encodings = {};
+
 
 	for(var i = 0; i < library.length; i++){ 			// loop over the two genres
 
@@ -132,20 +134,23 @@ function mainFunc(){
 				// AT THIS POINT, the artist string containing all lyrics for an artist is built, 
 				// USE IT NOW, it will be overwritten next time around
 				artists[artistName] = artistString; // example of saving it away in stupidObject
-                artists[artistName].encoding = compressionForString(artists[artistName]);
-				console.log("Saved " + artistName + " 's songs+encoding:");
+                encodings[artistName] = compressionForString(artists[artistName]);
+                //console.log(artistName);
+				//console.log("Saved " + artistName + " 's songs+encoding:");
+                //console.log(encodings[artistName].encoding);
 
 			}
 			// AT THIS POINT, the subgenre string containing all lyrics for a subgenre is built, 
 			// USE IT NOW, it will be overwritten next time around
             subgenres[subgenreName] = subgenreString;
-            subgenres[subgenreName].encoding = compressionForString(subgenres[subgenreName]);
+            encodings[subgenreName] = compressionForString(subgenres[subgenreName]);
+            //console.log("----------------------------------------" + subgenreName);
             //console.log(subgenres[subgenreName]);
 		}
 		// AT THIS POINT, the genre string containing all lyrics for a genre is built, 
 		// USE IT NOW, it will be overwritten next time around
         genres[genreName] = genreString;
-        genres[genreName].encoding = compressionForString(subgenres[subgenreName]);
+        encodings[genreName] = compressionForString(subgenres[subgenreName]);
 	}
 
     //Traverse all genres
@@ -183,14 +188,15 @@ function mainFunc(){
 
 
 	// example of huffman encoding a thing
-	var s = "How would it end If the truth was re-writable Break with past Whatever dreams you long for I've seen what the future has in mind for me Throwing the spear into the heart of the void Counting the odds Like I am out of control It's like a fight against the gravity Breaking the code's Like a mission impossible Letters that falls I'm only partly mechanical My strength is my weakness Please unfasten me Infinity, for a moment in time Will memories be revived I turn the page in the chapters of life Infinity keeps me alive Stuck in a wheel I'm alive it's a miracle What should I do about the pain is this critical You know that the future looks the same for me Watching my life Leaving everything inside of me While the sun Devours our history This time there's no turning back I leave it be! Infinity, for a moment in time Will memories be revived I turn the page in the chapters of life Infinity keeps me alive";
+	//var s = "How would it end If the truth was re-writable Break with past Whatever dreams you long for I've seen what the future has in mind for me Throwing the spear into the heart of the void Counting the odds Like I am out of control It's like a fight against the gravity Breaking the code's Like a mission impossible Letters that falls I'm only partly mechanical My strength is my weakness Please unfasten me Infinity, for a moment in time Will memories be revived I turn the page in the chapters of life Infinity keeps me alive Stuck in a wheel I'm alive it's a miracle What should I do about the pain is this critical You know that the future looks the same for me Watching my life Leaving everything inside of me While the sun Devours our history This time there's no turning back I leave it be! Infinity, for a moment in time Will memories be revived I turn the page in the chapters of life Infinity keeps me alive";
+    var s = "hello hello hello hello hello hello hello hello hello hello hello hello hello ";
     var result = compressionForString(s);
 
 	// result has the properties eight (compression ratio vs eight-bits-per-char)
 		// block (compression ratio vs block encoding)
 		// and encoding (the encoding object generated, or the one you specified)
 
-
+    console.log(result.encoding);
 	var encoding = result.encoding;  // EXAMPLE OF SAVING ONE ENCODING TO USE IT WITH NEXT HUFFMAN
 	console.log(result.eight);	// these console.logs should be the same, it means my code works
 
